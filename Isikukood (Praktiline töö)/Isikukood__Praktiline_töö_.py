d8bd24008491f5ec910d657f56ccb4e4628b3bf3
EkaterinaMosljajeva <120181296+EkaterinaMosljajeva@users.noreply.github.com>
2023-02-06 14:57:13 +0200

    Update Isikukood__Praktiline_töö_.py

--- Isikukood (Praktiline töö)/Isikukood__Praktiline_töö_.py
+++ Isikukood (Praktiline töö)/Isikukood__Praktiline_töö_.py
@@ -1,16 +1,33 @@
 from moodul import *
 
 ikood=""
+arvud=[]
+ikoodid=[]
 while True:
     ikood=input("Sisesta isikukood: ")
+    if ikood=="-":
+        break
     if pikkus(ikood)==False:
         print("Liiga pikk või lühike isikukood")
+        arvud.append(ikood)
     else:
         s=sugu(ikood)
         if s=="viga":
             print("Esimene täht ei ole õige")
+            arvud.append(ikood)
         else:
             if sunnipaev(ikood)=="viga":
                 print("2-7 tähed on vead")
+                arvud.append(ikood)
             else:
                 lause(ikood)
+                if kontrollnr(ikood)==int(ikood[-1]):
+                    print("OK")
+                    ikoodid.append(ikood)
+                else:
+                    print("!!!")
+print()
+ikoodid=naised_mehed(ikoodid)
+print(ikoodid)
+arvud=arvud_sorted(arvud)
+print(arvud)
